c6bdfec5d54b06ba07e92d2753193f3cd8d01970
Jean Perbet <jeanperbet@icloud.com>
2024-02-02 12:33:01 +0100

    issue #1 - feat: created the code skeleton with Gradle

---
+++ src/test/java/se/kth/ci/MainTest.java
@@ -0,0 +1,12 @@
+package se.kth.ci;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void fooTest(){
+        assertEquals(3, 2 + 1);
+    }
+}
